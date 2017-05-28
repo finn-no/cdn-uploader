0204f8bb54f6c5fc30e8198041b3ca5081f82e33
Simen Bekkhus <sbekkhus91@gmail.com>
2017-05-28 09:40:56 +0200

    Replace del, mkdirp and pify with fs-extra

--- test/lib/file-util.test.js
+++ test/lib/file-util.test.js
@@ -1,14 +1,8 @@
 import test from 'ava';
 import os from 'os';
-import fs from 'fs';
+import fs from 'fs-extra';
 import path from 'path';
-import pify from 'pify';
-import mkdirp from 'mkdirp';
-import del from 'del';
 import fileUtil from '../../lib/file-util.js';
-
-const mkdirpP = pify(mkdirp);
-const writeFile = pify(fs.writeFile);
 
 const workPath = path.join(os.tmpdir(), 'cdn-test');
 const file1 = path.join(workPath, 'test.txt');
@@ -16,15 +10,17 @@ const nested = path.join(workPath, 'nested');
 const file2 = path.join(nested, 'test.txt');
 const ignoredFile = path.join(workPath, '.ignore.txt');
 
-test.before(() => mkdirpP(nested)
-    .then(() => Promise.all([
-        writeFile(file1, 'woop'),
-        writeFile(file2, 'woop'),
-        writeFile(ignoredFile, ''),
-    ]))
-);
+test.before(async () => {
+    await fs.ensureDir(nested);
 
-test.after.always(() => del(workPath, { force: true }));
+    await Promise.all([
+        fs.writeFile(file1, 'woop'),
+        fs.writeFile(file2, 'woop'),
+        fs.writeFile(ignoredFile, ''),
+    ]);
+});
+
+test.after.always(() => fs.remove(workPath));
 
 test('should be a directory', t => {
     t.true(fileUtil.isDirectory(workPath));
